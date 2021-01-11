1d2688606e8832fa27fd79e037d8c78808f1b7df
Steve Good <sgood@lanctr.com>
2021-01-11 12:08:03 -0800

    Rewrote class as arrow function

--- src/components/tagWidget/TagControl.js
+++ src/components/tagWidget/TagControl.js
@@ -2,23 +2,28 @@
 
 import React from "react"
 
-export class TagControl extends React.Component {
-  handleChange = e => {
-    const separator = this.props.field.get("separator", ", ")
-    this.props.onChange(e.target.value.split(separator).map(e => e.trim()))
+export const TagControl = ({
+  field,
+  value,
+  forID,
+  classNameWrapper,
+  onChange,
+}) => {
+  const separator = field.get("separator", ", ")
+
+  const handleChange = e => {
+    onChange(e.target.value.split(separator).map(e => e.trim()))
   }
 
-  render() {
-    const separator = this.props.field.get("separator", ", ")
-    var value = this.props.value
-    return React.createElement("input", {
-      id: this.props.forID,
-      className: this.props.classNameWrapper,
-      type: "text",
-      value: value ? value.join(separator) : "",
-      onChange: this.handleChange,
-    })
-  }
+  return (
+    <input
+      id={forID}
+      className={classNameWrapper}
+      type="text"
+      value={value ? value.join(separator) : ""}
+      onChange={handleChange}
+    />
+  )
 }
 
 export default TagControl
