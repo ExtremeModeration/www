998313a068fa97d9992defacdefe7dcdab50a912
Steve Good <sgood@lanctr.com>
2021-01-28 15:45:48 -0800

    Cleaned up unused imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
-
-import Bio from "../components/user/bio"
+import { graphql } from "gatsby"
 import { Layout } from "../components/layout"
 import SEO from "../components/seo"
 import { BlogPostList } from "../components/blog"
